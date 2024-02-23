999b2315c4fc136a4f9af4b9f2d4d4940c0df248
Rob Cameron <cannikin@fastmail.com>
2024-02-22 20:31:40 -0800

    Add logo

--- web/src/layouts/AppLayout/AppLayout.jsx
+++ web/src/layouts/AppLayout/AppLayout.jsx
@@ -4,9 +4,12 @@ const AppLayout = ({ children }) => {
   return (
     <div className="mx-auto max-w-screen-lg pb-16 xl:max-w-screen-xl">
       <header className="px-4 pb-4 pt-6">
-        <h1 className="text-3xl font-semibold text-white">
-          <Link to={routes.home()} className="hover:underline">
-            photowood
+        <h1 className="flex items-start text-3xl font-semibold text-white">
+          <div className="flex h-8 w-8 items-center justify-center rounded-sm bg-white">
+            <div className="h-4 w-6 bg-neutral-700">&nbsp;</div>
+          </div>
+          <Link to={routes.home()} className="ml-2 hover:underline">
+            cambium
           </Link>
         </h1>
       </header>
