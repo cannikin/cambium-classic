b7d4b64a9769a1828991493390e0ccac3488c830
Rob Cameron <cannikin@fastmail.com>
2024-02-23 11:18:36 -0800

    Tailwind safelist possible filter values

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -1,4 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+
+const safelist = []
+
+// For 0 to 3 values in 0.01 increments
+for (let i = 0; i <= 3; i += 0.01) {
+  const value = Math.round(i * 100) / 100
+  safelist.push(`brightness-[${value}]`)
+  safelist.push(`contrast-[${value}]`)
+  safelist.push(`saturate-[${value}]`)
+}
+
+// For 0% to 100% values in 0.5 increments
+for (let i = 0; i <= 100; i += 0.5) {
+  const value = Math.round(i * 10) / 10
+  safelist.push(`grayscale-[${value}%]`)
+}
+
+// For 0 to 1 values in 0.01 increments
+for (let i = 0; i <= 1; i += 0.01) {
+  const value = Math.round(i * 100) / 100
+  safelist.push(`sepia-[${value}]`)
+}
+
+// For 0deg to 180deg values in 1 increments
+for (let i = 0; i <= 360; i += 1) {
+  safelist.push(`hue-rotate-[${i}deg]`)
+}
+
+console.info(safelist.filter((value) => value.match(/hue/)))
+
 module.exports = {
   content: ['src/**/*.{js,jsx,ts,tsx,html}'],
   theme: {
@@ -21,4 +51,5 @@ module.exports = {
     },
   },
   plugins: [],
+  safelist,
 }
