f75f4772c6e97d2fc45dbd2777095389db3454e3
Rob Cameron <cannikin@fastmail.com>
2024-02-26 15:34:17 -0800

    Move blank slate wrapper into separate component

---
+++ web/src/components/Blank/Blank.jsx
@@ -0,0 +1,9 @@
+const Blank = ({ children }) => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="text-center">{children}</div>
+    </div>
+  )
+}
+
+export default Blank
