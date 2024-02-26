72829440613e23b5b341530f30c25404456807d5
Rob Cameron <cannikin@fastmail.com>
2024-02-26 15:34:08 -0800

    Adds PhotoContext to router

--- web/src/Routes.jsx
+++ web/src/Routes.jsx
@@ -10,12 +10,13 @@
 import { Router, Route, Set } from '@redwoodjs/router'
 
 import { EditContextProvider } from 'src/contexts/EditContext'
+import { PhotosContextProvider } from 'src/contexts/PhotosContext'
 import AppLayout from 'src/layouts/AppLayout'
 
 const Routes = () => {
   return (
     <Router>
-      <Set wrap={[AppLayout, EditContextProvider]}>
+      <Set wrap={[PhotosContextProvider, EditContextProvider, AppLayout]}>
         <Route path="/" page={HomePage} name="home" />
         <Route path="/photos/{id:Int}/edit" page={EditPage} name="edit" />
         <Route notfound page={NotFoundPage} />
