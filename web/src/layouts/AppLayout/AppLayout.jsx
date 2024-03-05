fd24f49611c1ff792b5a67fff16e0973c6332397
Rob Cameron <cannikin@fastmail.com>
2024-03-05 11:48:03 -0800

    Fix logo rollover area

--- web/src/layouts/AppLayout/AppLayout.jsx
+++ web/src/layouts/AppLayout/AppLayout.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { flushSync } from 'react-dom'
 
-import { navigate, routes } from '@redwoodjs/router'
+import { Link, navigate, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 
 const AppLayout = ({ children }) => {
@@ -22,17 +24,19 @@ const AppLayout = ({ children }) => {
 
       <div className="mx-auto max-w-screen-lg pb-16 xl:max-w-screen-xl">
         <header className="px-4 pb-4 pt-6">
-          <h1 className="group flex items-start">
-            <div className="flex h-8 w-8 items-center justify-center rounded-sm bg-neutral-500 transition duration-150 group-hover:bg-white">
-              <div className="h-4 w-6 bg-neutral-800">&nbsp;</div>
+          <h1 className="flex items-start">
+            <div onClick={onClick} className="group flex items-center">
+              <div className="flex h-8 w-8 items-center justify-center rounded-sm bg-neutral-500 transition duration-150 group-hover:bg-white">
+                <div className="h-4 w-6 bg-neutral-800">&nbsp;</div>
+              </div>
+              <button
+                type="button"
+                onClick={onClick}
+                className="ml-2 text-3xl  font-semibold text-neutral-500 transition duration-150 group-hover:text-white"
+              >
+                cambium
+              </button>
             </div>
-            <button
-              type="button"
-              onClick={onClick}
-              className="ml-2 text-3xl  font-semibold text-neutral-500 transition duration-150 group-hover:text-white"
-            >
-              cambium
-            </button>
           </h1>
         </header>
         <main className="">{children}</main>
